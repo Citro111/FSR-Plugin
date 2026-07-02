02795806371c9afd12988b7b4fde6a481b074167
Enric <enric.breuer@gmail.com>
2026-07-02 20:53:44 +0200

    More Debugging

--- officehours/office-hours.php
+++ officehours/office-hours.php
@@ -162,10 +162,18 @@ function fsr_office_hours_search($search_term) {
         // Mitgliedsnamen ergänzen
         if (!empty($rule['member_ids'])) {
             $members = fsr_get_members_data();
+            echo '<pre>';
+                print_r($members);
+            echo '</pre>';
             foreach ($members as $member) {
                 if (in_array($member['id'], $rule['member_ids'], true)) {
-                    $searchable[] = $member['first_name'] ?? '';
-                    $searchable[] = $member['last_name'] ?? '';
+                    $searchable[] = $member['name'] ?? '';
+                    if (!empty($member['position'])) {
+                        $searchable[] = $member['position'];
+                    }
+                    if (!empty($member['team'])) {
+                        $searchable[] = $member['team'];
+                    }
                 }
             }
         }
