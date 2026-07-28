5f50739614921b9a92afb8a2661bd6b502e20e84
Citro111 <69390189+Citro111@users.noreply.github.com>
2026-07-28 18:58:34 +0200

    Site Selection in Calander

--- calendar/admin-template.php
+++ calendar/admin-template.php
@@ -89,22 +89,29 @@ function fsr_calendar_render_admin_interface() {
                                 ); ?>"
                             >
                         </td>
-                        <td>
+                       <td>
                             <select
                                 class="fsr-category-page-select"
                                 name="fsr_calendar_categories[<?php echo $index; ?>][page_id]"
+                                style="width:300px;"
                             >
                                 <option value="">-- Seite auswählen --</option>
                                 <?php
-                                if (!empty($category['page_id'])) {
-                                    $page = get_post($category['page_id']);
-                                    if ($page) {
-                                        echo '<option selected value="' .
-                                            esc_attr($page->ID) .
-                                            '">' .
-                                            esc_html($page->post_title) .
-                                            '</option>';
-                                    }
+                                $pages = get_pages([
+                                    'post_type'      => 'page',
+                                    'post_status'    => 'publish',
+                                    'sort_column'    => 'post_title',
+                                    'sort_order'     => 'ASC',
+                                    'number'         => 0,
+                                ]);
+
+                                foreach ($pages as $page) {
+                                    printf(
+                                        '<option value="%d"%s>%s</option>',
+                                        $page->ID,
+                                        selected(($category['page_id'] ?? 0), $page->ID, false),
+                                        esc_html($page->post_title)
+                                    );
                                 }
                                 ?>
                             </select>
@@ -124,34 +131,39 @@ function fsr_calendar_render_admin_interface() {
     
     <script>
         jQuery(document).ready(function($) {
+            let categoryIndex = $('#category-table-body tr').length;
             // Add a new category row
             $('#add-category-btn').click(function(e) {
                 e.preventDefault();
-                var newRow = '<tr>' +
-                    '<td><input type="text" name="fsr_calendar_categories[${categoryIndex}][name]" placeholder="Kategorie Name"></td>' +
-                    '<td><input type="text" name="fsr_calendar_categories[${categoryIndex}][additionalNames]" placeholder="Weitere Namen (optional)"></td>' +
-                    '<td>' + '<select class="fsr-category-page-select" name="fsr_calendar_categories[${categoryIndex}][page_id]">' + '<option value="">-- Seite auswählen --</option>' + '</select>' + '</td>' +
-                    '<td><button class="remove-category">Entfernen</button></td>' +
-                    '</tr>';
+                let newRow = 
+                <tr>
+                    <td><input type="text" name="fsr_calendar_categories[${categoryIndex}][name]" placeholder="Kategorie Name"></td>
+                    <td><input type="text" name="fsr_calendar_categories[${categoryIndex}][additionalNames]" placeholder="Weitere Namen (optional)"></td>
+                    <td><select class="fsr-category-page-select" name="fsr_calendar_categories[${categoryIndex}][page_id]" style="width:300px;">
+                            <option value="">-- Seite auswählen --</option>
+                            <?php
+                            $pages = get_pages([
+                                'post_type'   => 'page',
+                                'post_status' => 'publish',
+                                'sort_column' => 'post_title',
+                                'sort_order'  => 'ASC',
+                                'number'      => 0,
+                            ]);
+
+                            foreach ($pages as $page) {
+                                echo '<option value="' . esc_attr($page->ID) . '">' . esc_html($page->post_title) . '</option>';
+                            }
+                            ?>
+                        </select>
+                    </td>
+                    <td><button class="button remove-category">Entfernen</button></td>
+                </tr>
+            `;
                 $('#category-table tbody').append(newRow);
                 $('.fsr-category-page-select').select2({
                     width: '300px',
                     placeholder: 'Seite suchen...',
-                    minimumInputLength: 2,
-                    ajax: {
-                        url: ajaxurl,
-                        dataType: 'json',
-                        delay: 250,
-                        data: function(params) {
-                            return {
-                                action: 'fsr_search_pages',
-                                q: params.term
-                            };
-                        },
-                        processResults: function(data) {
-                            return data;
-                        }
-                    }
+                    allowClear: true
                 });
             });
             // Remove a category row
