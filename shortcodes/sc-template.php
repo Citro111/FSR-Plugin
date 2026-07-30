52b18d795d847746d560f75d3bbeba86338f448d
Citro111 <69390189+Citro111@users.noreply.github.com>
2026-07-31 00:25:39 +0200

    More testtss

--- shortcodes/sc-template.php
+++ shortcodes/sc-template.php
@@ -1,71 +1,83 @@
 <?php
 if (!defined('ABSPATH')) exit;
 ?>
+
 <div class="wrap">
     <h1>Shortcodes Übersicht</h1>
     <?php foreach ($shortcodes as $code => $data): ?>
-    <div class="card">
-        <h2>
-            <?php echo esc_html($data['title']); ?>
-        </h2>
-        <p>
-            <strong>[<?php echo esc_html($code); ?>]</strong>
-        </p>
-        <p>
-            <?php echo esc_html($data['description']); ?>
-        </p>
-        <h3>Beispiel</h3>
-        <code>
-            <?php echo esc_html($data['example']); ?>
-        </code>
-        <?php if (!empty($data['attributes'])): ?>
-        <h3>Attribute</h3>
-        <table class="widefat">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Beschreibung</th>
-                    <th>Möglichen Werte</th>
-                    <th>Default</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($data['attributes'] as $name=>$attr): ?>
-                <tr>
-                    <td>
-                        <?php echo esc_html($name); ?>
-                    </td>
-                    <td>
-                        <?php echo esc_html($attr['description']); ?>
-                    </td>
-                    <td>
-                        <?php echo esc_html($attr['values']); ?>
-                    </td>
-                    <td>
-                        <?php echo esc_html($attr['default'] ?? ''); ?>
-                    </td>
-                </tr>
+    <details class="fsr-shortcode-accordion">
+        <summary>
+            <strong><?php echo esc_html($data['title']); ?></strong>
+            <code>[<?php echo esc_html($code); ?>]</code>
+            <?php if (!empty($data['description'])): ?>
+                <span>
+                    - <?php echo esc_html($data['description']); ?>
+                </span>
+            <?php endif; ?>
+        </summary>
+        <div class="fsr-shortcode-content">
+            <h3>Shortcode</h3>
+            <code>
+                <?php echo esc_html('[' . $code . ']'); ?>
+            </code>
+            <?php if (!empty($data['example'])): ?>
+                <h3>Beispiel</h3>
+                <code>
+                    <?php echo esc_html($data['example']); ?>
+                </code>
+            <?php endif; ?>
+            <?php if (!empty($data['attributes'])): ?>
+            <h3>Attribute</h3>
+            <table class="widefat striped">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Beschreibung</th>
+                        <th>Mögliche Werte</th>
+                        <th>Default</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($data['attributes'] as $name => $attr): ?>
+                    <tr>
+                        <td>
+                            <code><?php echo esc_html($name); ?></code>
+                        </td>
+                        <td>
+                            <?php echo esc_html($attr['description'] ?? ''); ?>
+                        </td>
+                        <td>
+                            <?php echo esc_html($attr['values'] ?? ''); ?>
+                        </td>
+                        <td>
+                            <?php echo esc_html($attr['default'] ?? ''); ?>
+                        </td>
+                    </tr>
                 <?php endforeach; ?>
-            </tbody>
-        </table>
-        <?php endif; ?>
-        <h3>Verwendet auf:</h3>
-        <?php if (!empty($usage[$code])): ?>
-        <ul>
-            <?php foreach ($usage[$code] as $place): ?>
-            <li>
-                <a href="<?php echo esc_url($place['edit']); ?>">
-                    <?php echo esc_html($place['title']); ?>
-                </a>
-                (<?php echo esc_html($place['type']); ?>)
-            </li>
-            <?php endforeach; ?>
-        </ul>
-        <?php else: ?>
-        <p>
-            Noch nicht verwendet.
-        </p>
-        <?php endif; ?>
-    </div>
+                </tbody>
+            </table>
+            <?php endif; ?>
+            <h3>Verwendet auf:</h3>
+            <?php if (!empty($usage[$code])): ?>
+                <ul>
+                <?php foreach ($usage[$code] as $place): ?>
+                    <li>
+                        <a href="<?php echo esc_url($place['edit']); ?>">
+                            <?php echo esc_html($place['title']); ?>
+                        </a>
+                        <small>
+                            (<?php echo esc_html($place['type']); ?>,
+                            <?php echo esc_html($place['status']); ?>)
+                        </small>
+                    </li>
+                <?php endforeach; ?>
+                </ul>
+            <?php else: ?>
+                <p>
+                    Noch nicht verwendet.
+                </p>
+            <?php endif; ?>
+        </div>
+    </details>
     <?php endforeach; ?>
 </div>
